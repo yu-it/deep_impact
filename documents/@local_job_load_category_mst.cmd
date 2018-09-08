c1693c89aeb3102b29f1c6c102dbb773c4abcf90
yu-it <yousuke0303@gmail.com>
2018-09-08 18:23:50 +0900

    ひとしきりできた。kyiにパーティショニング用疑似列を挿入前

--- documents/@local_job_load_category_mst.cmd
+++ documents/@local_job_load_category_mst.cmd
@@ -1,3 +1,4 @@
 cmd /c bq rm -t -f jrdb_raw_data_schema_info.category_mst
+cmd /c gsutil -o GSUtil:default_project_id=yu-it-base cp assets\bigquery\definition_data\csv\category_mst.csv gs://deep_impact/assets/bigquery/definition_data/csv/
 cmd /c bq load  --skip_leading_rows=1 jrdb_raw_data_schema_info.category_mst gs://deep_impact/assets/bigquery/definition_data/csv/category_mst.csv assets\bigquery\definition_data\schema_info_ddl\category_mst.json
 timeout 10
