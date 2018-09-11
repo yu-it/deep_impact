d7bebefca41f35523d0cf8c253b7dbaa0479441c
yu-it <yousuke0303@gmail.com>
2018-09-12 08:53:25 +0900

    commit

--- documents/@local_job_deploy_detail.cmd
+++ documents/@local_job_deploy_detail.cmd
@@ -1,3 +1,3 @@
 cmd /c bq rm -t jrdb_raw_data.%1
-cmd /c bq mk --time_partitioning_field __partitioning_date --table jrdb_raw_data.%1 assets\bigquery\definition_data\ddl\jrdb_raw_data\%1.json
+cmd /c bq mk --time_partitioning_field partitioning_date --table jrdb_raw_data.%1 assets\bigquery\definition_data\ddl\jrdb_raw_data\%1.json
 timeout 10
